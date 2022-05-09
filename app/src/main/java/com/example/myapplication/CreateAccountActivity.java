12a06e3efdedb22b0c825eec6948694287b4b9af
zgonnikov01 <zgonnikov01@gmail.com>
2022-05-09 13:32:21 +0300

    log in and sign in buttons fixed, app icon changed

--- app/src/main/java/com/example/myapplication/CreateAccountActivity.java
+++ app/src/main/java/com/example/myapplication/CreateAccountActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,7 +28,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_sign_up);
         mAuth = FirebaseAuth.getInstance();
         emailET = findViewById(R.id.signInEmail);
         passwordET = findViewById(R.id.signInPassword);
